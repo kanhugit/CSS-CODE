6249166d7274674104896722901cef1f93c66b9e
KANHU CHARAN BEHERA <beherakanha029@gmail.com>
2025-04-02 16:40:57 +0530

    Add files via upload

---
+++ Resume/script.js
@@ -0,0 +1,7 @@
+// console.log(10-"10");//0
+// console.log("20"-"20");//0
+// console.log(10+"2");//102
+// console.log(10*"2");//20
+// console.log(typeof(NaN))//number
+// console.log(10-"hello")//NaN
+
